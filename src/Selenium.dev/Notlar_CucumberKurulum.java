119d852840be217150428c7fe463a18c28eea428
Alparslan <maslan11alp@gmail.com>
2022-11-02 20:07:54 +0300

    20221102

---
+++ src/Selenium.dev/Notlar_CucumberKurulum.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+public class Notlar_CucumberKurulum {
+    /*
+    --> POM.xml'de Selenium Java, WebDriverManager, Cucumber Java, Cucumber JUnit dependincies leri olmalidir.
+    --> Test-Java klasorunun altina Pages, Runners, StepDefinitions ve Utilities package'lari acilir.
+    --> Test package'inin altina resources directory acilir ve icine Features uzantili Java kodu olmayan
+        Gherkin dilinde test case'ler yazilir, yazilan test case'lerin Java kodlarini elde etmek icin
+        uzerine gelince acilan pencereden ya da sayfayi run ederek konsoldan alabiliriz.
+    --> StepDefinitions icine Java kodlarini yazariz.
+    --> Run ettigimizde hata aliyorsak cekici kullanabiliriz.(Yapilandirma yapiyor).
+    --> Utilities package'i icine Driver ve ConfigReader classlari konulur.(Diger projelerden alinabilir)
+    --> Given, When, And, Then kullanirken Java kodu acisindan farki yoktur sadece okuyanin daha iyi anlayabilmesi
+        icin kullanilir.
+    --> Features class'i icinde ilk satira  Feature: yazilarak baslanir.(Feature: Amazon Search)
+            - Altina Scenario: yazilarak devam edilir.(Scenario: TC01 Kullanici amazonda nutella aratir)
+
+     */
+}
